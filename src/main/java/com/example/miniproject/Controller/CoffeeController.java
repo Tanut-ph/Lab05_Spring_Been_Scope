a506addbaeb5913d480918019900b39cd79a326b
zamzibedsingha <ratima44326@gmail.com>
2026-07-26 22:26:22 +0700

    add post put delete get

--- src/main/java/com/example/miniproject/Controller/CoffeeController.java
+++ src/main/java/com/example/miniproject/Controller/CoffeeController.java
@@ -5,6 +5,10 @@ import com.example.miniproject.Service.CoffeeService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import java.util.List;
 
 @RestController
@@ -21,5 +25,19 @@ public class CoffeeController {
     @GetMapping("/coffees/{id}")
     public Coffee getCoffeeById(@PathVariable int id) {
         return coffeeService.getById(id);
+    }
+    @PostMapping("/coffees")
+    public Coffee addCoffee(@RequestBody Coffee coffee) {
+        return coffeeService.addCoffee(coffee);
+    }
+    @PutMapping("/coffees/{id}")
+    public Coffee updateCoffee(@PathVariable int id,
+                           @RequestBody Coffee coffee) {
+
+        return coffeeService.updateCoffee(id, coffee);
+    }
+    @DeleteMapping("/coffees/{id}")
+    public boolean deleteCoffee(@PathVariable int id) {
+        return coffeeService.deleteCoffee(id);
     }
 }
